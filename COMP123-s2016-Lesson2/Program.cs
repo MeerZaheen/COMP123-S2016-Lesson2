d22a9bd8c471710e0ac2c8dce4a9dd954dbc4607
meerzaheen_90 <meerzaheen_90@hotmail.com>
2016-05-17 12:55:19 -0400

    Added reference to driver class from UnitTest

--- COMP123-s2016-Lesson2/Program.cs
+++ COMP123-s2016-Lesson2/Program.cs
@@ -4,10 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* 
+* Author: Meer Zaheen Nazmul
+* Date:May 17, 2016
+* Date Modified: May 17, 2016
+* Description: Advanced Method Demo Lesson 2
+* Version: 0.0.2 - Added reference to driver class from UnitTest
+*/
+
 namespace COMP123_s2016_Lesson2
 {
+    /**
+    * Main Driver Class for Lesson 2
+    * 
+    * @class Program
+    */
     class Program
     {
+        /**
+        * Main method for our driver class
+        *
+        * @constructior main
+        * @param {string[]} args
+        */
         static void Main(string[] args)
         {
         }
